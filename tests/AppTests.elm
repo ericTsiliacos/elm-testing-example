ac5097e1e5a27402abe8cd9bf9fca42df72d9e2f
Eric Tsiliacos <erictsiliacos@gmail.com>
2018-08-02 23:50:35 -0700

    support multiple expectations

--- tests/AppTests.elm
+++ tests/AppTests.elm
@@ -5,7 +5,7 @@ import Test exposing (..)
 import Test.Html.Query as Query exposing (..)
 import Test.Html.Event as Event exposing (..)
 import Test.Html.Selector exposing (..)
-import TestSupport exposing (uiTest, testView, expect)
+import TestSupport exposing (uiTest, testView, expect, andAlso, andThenExpect, seed, TestData, execute, runTests)
 import Result exposing (andThen, map)
 import App exposing (..)
 
@@ -54,6 +54,10 @@ run =
     uiTest App.view App.update
 
 
+foo =
+    seed App.view App.update
+
+
 suite : Test
 suite =
     describe "Application"
@@ -67,38 +71,58 @@ suite =
         , describe "clicking on the increment button"
             [ test "increments the current count by 1" <|
                 \_ ->
-                    model
-                        |> run clickIncrement
-                        |> andThen (run clickIncrement)
-                        |> andThen (run clickIncrement)
-                        |> TestSupport.expect
-                            (view
-                                >> counter
-                                >> has [ text "3" ]
+                    (execute
+                        clickIncrement
+                        (foo model)
+                        (counter
+                            >> has [ text "1" ]
+                        )
+                    )
+                        |> Result.andThen
+                            (\testData ->
+                                execute
+                                    clickIncrement
+                                    (foo testData.model)
+                                    (counter
+                                        >> has [ text "2" ]
+                                    )
                             )
+                        |> Result.andThen
+                            (\testData ->
+                                execute
+                                    clickDecrement
+                                    (foo testData.model)
+                                    (counter
+                                        >> has [ text "1" ]
+                                    )
+                            )
+                        |> runTests
             ]
-        , describe "clicking on the decrement button"
-            [ test "decrements the current count by 1" <|
-                \_ ->
-                    { model | count = 3 }
-                        |> run clickDecrement
-                        |> andThen (run clickDecrement)
-                        |> TestSupport.expect
-                            (view
-                                >> Expect.all
-                                    [ counter >> has [ text "1" ]
-                                    , multiplier >> has [ text "2" ]
-                                    ]
-                            )
-            , test "counter does not go below 0" <|
-                \_ ->
-                    { model | count = 1 }
-                        |> run clickDecrement
-                        |> andThen (run clickDecrement)
-                        |> TestSupport.expect
-                            (view
-                                >> counter
-                                >> has [ text "0" ]
-                            )
-            ]
+          -- , describe "clicking on the decrement button"
+          --     [ test "decrements the current count by 1" <|
+          --         \_ ->
+          --             { model | count = 3 }
+          --                 |> run clickDecrement
+          --                 |> andThen (run clickDecrement)
+          --                 |> TestSupport.expect
+          --                     (view
+          --                         >> Expect.all
+          --                             [ counter >> has [ text "1" ]
+          --                             , multiplier >> has [ text "2" ]
+          --                             ]
+          --                     )
+          --     , test "counter does not go below 0" <|
+          --         \_ ->
+          --             { model | count = 1 }
+          --                 |> run clickDecrement
+          --                 |> andThen (run clickDecrement)
+          --                 |> TestSupport.expect
+          --                     (view
+          --                         >> counter
+          --                         >> has [ text "0" ]
+          --                     )
+          --     , test "foo" <|
+          --         \_ ->
+          --             1 |> Expect.equal 1 |> andAlso (2 |> Expect.equal 2)
+          --     ]
         ]
--- tests/TestSupport.elm
+++ tests/TestSupport.elm
@@ -2,7 +2,60 @@ module TestSupport exposing (..)
 
 import Test.Html.Query exposing (fromHtml)
 import Test.Html.Event exposing (toResult)
-import Expect
+import Expect exposing (Expectation)
+import Html exposing (Html)
+
+
+type alias TestData model msg =
+    { expectations : List Expectation
+    , view : model -> Html msg
+    , model : model
+    , update : msg -> model -> ( model, Cmd msg )
+    }
+
+
+seed view update model =
+    { expectations = [ Expect.pass ]
+    , view = view
+    , update = update
+    , model = model
+    }
+
+
+execute library testData expectation =
+    let
+        view =
+            testData.view testData.model
+
+        updatedModel =
+            (fromHtml view)
+                |> library
+                |> toResult
+                |> Result.map (testUpdate testData.update testData.model)
+    in
+        Result.map
+            (\model ->
+                model
+                    |> testData.view
+                    |> fromHtml
+                    |> expectation
+                    |> (\newExpectation -> { testData | expectations = newExpectation :: testData.expectations, model = model })
+            )
+            updatedModel
+
+
+runTests result =
+    case result of
+        Ok testData ->
+            List.foldr andAlso Expect.pass testData.expectations
+
+        Err error ->
+            Expect.fail error
+
+
+andAlso : Expectation -> Expectation -> Expectation
+andAlso l r =
+    Expect.all [ always l, always r ] ()
 
 
 testUpdate update model msg =
@@ -13,18 +66,32 @@ testView view model =
     fromHtml (view model)
 
 
-expect expectation =
-    \model ->
-        let
-            result =
-                Result.map expectation model
-        in
-            case result of
-                Ok expectation ->
-                    expectation
+andThenExpect : (a -> Expectation) -> (Result String a -> Expectation)
+andThenExpect expectation model =
+    let
+        result =
+            Result.map expectation model
+    in
+        case result of
+            Ok expectation ->
+                expectation
 
-                Err error ->
-                    Expect.fail error
+            Err error ->
+                Expect.fail error
+
+
+expect : (a -> Expectation) -> (Result String a -> Expectation)
+expect expectation model =
+    let
+        result =
+            Result.map expectation model
+    in
+        case result of
+            Ok expectation ->
+                expectation
+
+            Err error ->
+                Expect.fail error
 
 
 uiTest view update library model =
